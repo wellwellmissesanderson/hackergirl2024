2ab0303e31b7213542e79ce4d4c3a24e13ae932a
Heather Anderson <heather.anderson@coconutsoftware.com>
2024-05-09 00:41:20 -0700

    quick save before adding react

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './app.css';
 
 console.log('hey')
 
+// move background
 let on = false;
 document.getElementById('toggle').addEventListener('click', e => {
   on = !on;
@@ -9,3 +10,12 @@ document.getElementById('toggle').addEventListener('click', e => {
 
   document.getElementById('sliding-background').classList.toggle('slide');
 })
+
+// penguin jump
+const penguin = document.getElementById('penguin');
+penguin.addEventListener('click', e => {
+  penguin.classList.toggle('penguin-standing');
+  penguin.classList.toggle('penguin-jumping');
+})
+
+// https://css-tricks.com/books/fundamental-css-tactics/infinite-scrolling-background-image
