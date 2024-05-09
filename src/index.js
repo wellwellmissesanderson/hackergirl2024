7ef5816557860ff83e8b116dade4550e8f561731
Heather Anderson <heather.anderson@coconutsoftware.com>
2024-05-09 00:06:51 -0700

    Get moving background and button to toggle on/off

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './app.css';
 
 console.log('hey')
+
+let on = false;
+document.getElementById('toggle').addEventListener('click', e => {
+  on = !on;
+  console.log('clicked', on);
+
+  document.getElementById('sliding-background').classList.toggle('slide');
+})
