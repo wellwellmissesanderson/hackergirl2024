b1f2c6250b4c3419d759375ec7d5c086d2d74826
Heather Anderson <heather.anderson@coconutsoftware.com>
2024-05-12 13:37:35 -0700

    Add reducer instead of state

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 
+const reducer = (state, action) => {
+  console.log({ state, action })
+  switch (action.type) {
+    case 'TOGGLE_GAME':
+      return {
+        ...state,
+        playing: !state.playing,
+      }
+    case 'PENGUIN_JUMP':
+      return {
+        ...state,
+        penguinJumping: !state.penguinJumping,
+      }
+    default:
+      return { ...state };
+  }
+}
 
 const App = () => {
-  const [playing, setPlaying] = useState(false);
-  const [jumping, setJumping] = useState(false);
+  const [state, dispatch] = useReducer(reducer, {
+    penguinJumping: false,
+    playing: false,
+  });
+
+  console.log({ state });
 
   const handleBgClick = (e) => {
-    setPlaying(prevState => !prevState);
+    dispatch({ type: 'TOGGLE_GAME' })
   }
 
-  const handleJumping = (e) => {
-    setJumping(prevState => !prevState);
+  const handlePenguinJump = (e) => {
+    if (!state.playing) {
+      return;
+    }
+
+    dispatch({ type: 'PENGUIN_JUMP' })
   }
 
   return (
     <div className="overflow-hidden relative">
       <div
-        className={`sliding-background ${playing ? 'slide' : ''}`}
+        className={`sliding-background ${state.playing ? 'slide' : ''}`}
       ></div>
 
       <div className="inner absolute w-screen top-0 left-0 p-4 border-2 border-pink-700 z-10 flex flex-col">
         {/* Game Controls */}
-        <div className="flex  h-12 border-2 border-teal-500">
-          <button type="button" onClick={handleBgClick}>
+        <div className="flex justify-center h-12 border-2 border-teal-500">
+          <button type="button" onClick={handleBgClick} className="w-80 inline-block bg-yellow-400 border-2 border-purple-900">
             Start/Stop
           </button>
         </div>
@@ -30,20 +55,20 @@ const App = () => {
         {/* Scene */}
         <div className="border-2 h-full border-orange-700 flex">
           {/* Penguin  */}
-          <button className="border-2 border-purple-500 w-1/3 self-end" type="button" onClick={handleJumping}>
+          <button className="border-2 border-purple-500 w-1/3 self-end" type="button" onClick={handlePenguinJump}>
             <img
               className="max-h-44 inline-block"
-              src={`./assets/penguin-${jumping ? 'jump' : 'stand'}.png`}
-              alt={`penguin ${jumping ? 'jumping' : 'standing'}`}
+              src={`./assets/penguin-${state.penguinJumping ? 'jump' : 'stand'}.png`}
+              alt={`penguin ${state.penguinJumping ? 'jumping' : 'standing'}`}
             />
           </button>
 
           {/* Polar Bear  */}
           <button className="border-2 border-purple-500 w-2/3 self-end" type="button">
             <img
-              className="max-h-72 inline-block"
-              src={`./assets/penguin-${jumping ? 'jump' : 'stand'}.png`}
-              alt={`penguin ${jumping ? 'jumping' : 'standing'}`}
+              className="h-56 inline-block"
+              src={`./assets/polar_bear.png`}
+              alt={`polar bear`}
             />
           </button>
         </div>
